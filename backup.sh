6a6870495b7f66a6798ba3bfad36df08258db96e
Jessy SOBREIRO <jessy.sobreiro@gmail.com>
2021-03-31 00:50:53 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 TIMESTAMP=`date +%Y-%m-%d__%H-%M-%S`
-BACKUP_FOLDER_ID="<FOLDER_ID_FROM_GDRIVE>"
+BACKUP_FOLDER_ID="<GDRIVE-ID>"
 
 # appdata folders to backup (separated by spaces)
-APP_LIST=""
+APP_LIST="<FOLDERS SEPARATED BY SPACES>"
 
 # docker-appdata folders to backup (separated by spaces)
-DAPP_LIST=""
+DAPP_LIST="<FOLDERS SEPARATED BY SPACES>"
 
 if [ ! -f "/mnt/appdata/.itworks" ]; then
     echo "/mnt/appdata : not mounted, not backing up"
@@ -22,15 +22,18 @@ fi
 BACKUP_FOLDER="backup-$TIMESTAMP"
 
 mkdir /opt/backup/data/$BACKUP_FOLDER
-mkdir /opt/backup/data/$BACKUP_FOLDER/docker-appdata
+mkdir /opt/backup/data/$BACKUP_FOLDER/docker
+mkdir /opt/backup/data/$BACKUP_FOLDER/docker/appdata
 mkdir /opt/backup/data/$BACKUP_FOLDER/appdata
 
 for service in $DAPP_LIST; do
-    tar --exclude="<FOLDER_TO_EXCLUDE>" -cvjf - /mnt/docker-appdata/$service | gpg --trust-model always -e -r <GPG_KEY_ID> -o /opt/backup/data/$BACKUP_FOLDER/appdata/$service.tar.bz2.gpg
+    echo "BACKUP : /mnt/docker-appdata/$service"
+    tar --exclude="<FOLDER_TO_EXCLUDE>" -cjf - /mnt/docker-appdata/$service | gpg --trust-model always -e -r <KEY_ID> -o /opt/backup/data/$BACKUP_FOLDER/docker/$service.tar.bz2.gpg
 done
 
 for service in $APP_LIST; do
-    tar --exclude="node_modules" --exclude=".npm" -cvjf - /mnt/appdata/$service | gpg --trust-model always -e -r <GPG_KEY_ID> -o /opt/backup/data/$BACKUP_FOLDER/docker-appdata/$service.tar.bz2.gpg 
+    echo "BACKUP : /mnt/appdata/$service"
+    tar --exclude="node_modules" --exclude=".npm" -cjf - /mnt/appdata/$service | gpg --trust-model always -e -r <KEY_ID> -o /opt/backup/data/$BACKUP_FOLDER/appdata/$service.tar.bz2.gpg 
 done
 
 gdrive sync upload /opt/backup/data/ $BACKUP_FOLDER_ID
